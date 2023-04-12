b3ec5f9eb856e100ee83ce506f2bb3fb79156fc5
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2023-04-11 23:35:12 -0300

    Update hourlyChart.jsx

--- src/hourlyChart.jsx
+++ src/hourlyChart.jsx
@@ -11,42 +11,93 @@ function HourlyChart() {
 	}
 	const hourlyData = weatherData?.hourly;
 
-	console.log(hourlyData.time);
-
+	// console.log(hourlyData.time);
+	const hours = hourlyData?.time.map((hour) => {
+		const split = hour.split("T");
+		// console.log(split[1]);
+		// split =
+		return split[1];
+	});
+	// let hoursday = hours.slice(0, 24);
+	// console.log(hours);
 	const seriesx = [
 		{
 			name: "hours",
-			data: hourlyData?.time.map((hour) => {
-				const split = hour.split("T");
-				console.log(split[1]);
-				return split[1];
-			}),
+			data: hours.slice(0, 32).map((hour) => hour.split(":")[0]),
 		},
 	];
+	console.log(seriesx);
 	const seriesy = [
 		{
-			name: "temp",
-			data: hourlyData.temperature_2m,
+			name: "temperature",
+			data: hourlyData.temperature_2m.slice(0, 32),
 		},
 	];
 	const options = {
 		chart: {
 			id: "line-chart",
+			type: "area",
+
 			toolbar: {
 				show: true,
+				tools: {
+					selection: true,
+					zoom: true,
+					zoomin: true,
+					zoomout: true,
+					pan: true,
+					reset: true,
+				},
 			},
+			// background: "#ffff"
+			// dropShadow: {
+			// 	enabled: true,
+			// 	left: 0,
+			// 	blur: 6,
+			// },
+		},
+		theme: {
+			palette: "palette5", // upto palette10
+		},
+		
+		// fill: {
+		// 	type: "gradient",
+
+		// 	gradient: {
+		// 		shadeIntensity: 1,
+		// 		opacityFrom: 0.5,
+		// 		opacityTo: 0.8,
+		// 		stops: [0, 90, 30],
+		// 	}
+		// },
+		dataLabels: {
+			enabled: false,
 		},
 		xaxis: {
-			categories: seriesx,
+			type: "datetime",
+			tickAmount: 6,
+			tickPlacement: 'between',
+			categories: hourlyData.time.slice(0, 32),
+			// tooltip: {
+			// 	x: {
+			// 		format: "DD/MM/yy HH:MM",
+			// 	},
+				
+				
+			// },
+			
 		},
 		yaxis: {
 			title: {
 				text: "temp",
 			},
 		},
+		stroke: {
+			curve: "smooth",
+		},
 	};
 
-	return <Chart options={options} series={seriesy} />;
+	return <Chart options={options} series={seriesy} type='area' width={500} />;
 }
 
 export default HourlyChart;
